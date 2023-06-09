6f1c025f62660ddb21ec03517432a4d58dbc9c4a
winstxnhdw <winstxnhdw@gmail.com>
2023-06-09 08:36:18 +0800

    chore(partition): add type documentation

--- server/features/partition.py
+++ server/features/partition.py
@@ -1,21 +1,34 @@
-from typing import BinaryIO, Literal, Optional, Union
+from typing import BinaryIO, Optional
 
 from pdfminer.pdfparser import PDFSyntaxError
 from starlette.exceptions import HTTPException
 from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
 from unstructured.partition.auto import partition
 
+from server.features.types import Strategy
+
 
 def unstructured_partition(
     file: BinaryIO,
     file_name: Optional[str] = None,
     file_type: Optional[str] = 'Unknown',
-    strategy: Optional[Union[Literal['hi_res'], Literal['fast']]] = 'hi_res'
+    strategy: Optional[Strategy] = 'hi_res'
 ) -> str:
     """
     Summary
     -------
     this function uses the unstructured library to partition a file
+
+    Parameters
+    ----------
+    file (BinaryIO): the file to partition
+    file_name (Optional[str]): the name of the file
+    file_type (Optional[str]): the type of the file
+    strategy (Optional[Strategy]): the strategy to use
+
+    Returns
+    -------
+    extracted_text (str): the extracted text
     """
     try:
         return '\n\n'.join(str(element) for element in partition(file=file, strategy=strategy))
---
+++ server/features/types/strategy.py
@@ -0,0 +1,3 @@
+from typing import Literal
+
+Strategy = Literal['hi_res', 'fast']
